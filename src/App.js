58af9fce7a5369965f71496711ffd1bc1129eaae
Skhron <kgolubkov0@gmail.com>
2022-04-22 16:03:57 +0300

    list added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import ClassCounter from "./component/ClassCounter";
 import PostItem from "./component/PostItem";
+import PostList from "./component/PostList";
 import './styles/App.css'
 
 function App() {
-  const [value, setValue] = useState('Text')
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript 1', body: 'Description'},
+    {id: 2, title: 'Javascript 2', body: 'Description'},
+    {id: 3, title: 'Javascript 3', body: 'Description'}
+  ])
 
-  
+
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}}/>
+      <PostList posts={posts} title="List of posts(1)"/>
     </div>
   );
 }
