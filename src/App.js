81501b82e3b64494bdb01f98a49e51c728ee99b1
Skhron <kgolubkov0@gmail.com>
2022-04-22 13:09:34 +0300

    State inc/dec

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import Counter from "./component/Counter";
 
 function App() {
+  const [value, setValue] = useState('Text')
+
+  
   return (
     <div className="App">
-      
+      <Counter/>
     </div>
   );
 }
